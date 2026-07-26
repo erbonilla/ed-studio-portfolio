224c64f9511a04abbf6a219a13425d802472d0e2
Ed Bonilla <edbonilla@192.168.1.6>
2026-07-26 10:24:46 -0600

    Place About before selected work

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
       <IntroLoader />
       <PortfolioNavigation />
       <PortfolioHero />
-      <WorkSection />
       <AboutSection />
+      <WorkSection />
     </main>
   );
 }
